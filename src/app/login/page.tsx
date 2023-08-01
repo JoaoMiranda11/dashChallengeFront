b3f1f17e3985c22f2c21a03fa705390652806d91
João Miranda <jsm2.pe@gmail.com>
2023-08-01 19:03:32 -0300

    #main [FIX] background login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 import { Button } from "@/components/inputs/button";
 import { useSession } from "@/hooks/useSession";
 import { useCallback, useState } from "react";
+import styles from './styles.module.css'
 
 export default function Login() {
     const [ loading, setLoading ] = useState(false);
@@ -24,9 +25,12 @@ export default function Login() {
     }, [singIn])
     
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="w-full max-w-xs">
-                <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit(submit)}>
+        <main className={styles.pageLayout}>
+            <div className={styles.formArea}>
+                <div className={styles.session} >
+                    <div className={styles.background} />
+                </div>
+                <form className={styles.form} onSubmit={handleSubmit(submit)}>
                     <TextInput control={control} name="username" label="Username" required regex={/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/} />
                     <TextInput control={control} name="password" label="Password" hide required />
                     <div className="flex items-center justify-center">
